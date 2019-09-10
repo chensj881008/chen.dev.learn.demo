058dc1dbc22aca42160a0b03ac8295a505e2f293
chensj(陈世杰) <chensj@winning.com.cn>
2019-09-10 17:08:43 +0800

    迁移页面

--- spring-security4/spring-sercurity-ssm/src/main/java/org/chen/spring/security/ssm/mapper/SysUserDao.java
+++ spring-security4/spring-sercurity-ssm/src/main/java/org/chen/spring/security/ssm/mapper/SysUserDao.java
@@ -1,72 +1,92 @@
 package org.chen.spring.security.ssm.mapper;
 
 
+import java.util.List;
 
-import java.util.List;  
+import org.springframework.dao.DataAccessException;
 
-import org.springframework.dao.DataAccessException;  
-
-import org.chen.spring.security.ssm.domain.SysUser;  
-
+import org.chen.spring.security.ssm.domain.SysUser;
 
 
 import org.springframework.stereotype.Repository;
+
 /**
-* @author Dev Ops
-* @title 用户表DAO接口
-* @email Winning Devops Apollo Client
-* @package org.chen.spring.security.ssm.mapper
-* @date 2019-09-09 22:14:05
-*/
+ * @author Dev Ops
+ * @title 用户表DAO接口
+ * @email Winning Devops Apollo Client
+ * @package org.chen.spring.security.ssm.mapper
+ * @date 2019-09-09 22:14:05
+ */
 @Repository
 public interface SysUserDao {
     /**
      * SysUser 数据新增
+     *
      * @param sysUser
      * @return int
      * @throws DataAccessException
      */
     public int insertSysUser(SysUser sysUser) throws DataAccessException;
+
     /**
      * SysUser 数据更新
+     *
      * @param sysUser
      * @return int
      * @throws DataAccessException
      */
     public int updateSysUser(SysUser sysUser) throws DataAccessException;
+
     /**
      * SysUser 数据删除
+     *
      * @param sysUser
      * @return int
      * @throws DataAccessException
      */
     public int deleteSysUser(SysUser sysUser) throws DataAccessException;
+
     /**
      * SysUser 单条数据查询
+     *
      * @param sysUser
      * @return SysUser
      * @throws DataAccessException
      */
     public SysUser selectSysUser(SysUser sysUser) throws DataAccessException;
+
     /**
      * SysUser 统计数据条数
+     *
      * @param sysUser
      * @return Object
      * @throws DataAccessException
      */
     public int selectSysUserCount(SysUser sysUser) throws DataAccessException;
+
     /**
      * SysUser 查询List
+     *
      * @param sysUser
      * @return SysUserList
      * @throws DataAccessException
      */
     public List<SysUser> selectSysUserList(SysUser sysUser) throws DataAccessException;
+
     /**
      * SysUser 分页查询
+     *
      * @param sysUser
      * @return SysUserList
      * @throws DataAccessException
      */
     public List<SysUser> selectSysUserPageList(SysUser sysUser) throws DataAccessException;
+
+    /**
+     * 根据username获取用户信息
+     *
+     * @param username 用户名
+     * @return user
+     */
+    public SysUser findUserByUsername(String username);
 }
