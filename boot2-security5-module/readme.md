d1389976175e420cb2090b8be7509894584cf874
chensj(陈世杰) <chensj@winning.com.cn>
2019-09-12 09:08:50 +0800

    SpringBoot集成Spring Security——自动登录：数据库方式存储

--- boot2-security5-module/readme.md
+++ boot2-security5-module/readme.md
@@ -596,3 +596,92 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 #### 2.2.2 数据库方式存储
 
+​		使用 Cookie 存储虽然很方便，但是大家都知道**Cookie 毕竟是保存在客户端的**，而且**Cookie 的值还与用户名、密码这些敏感数据相关**，虽然加密了，但是将敏感信息存在客户端，毕竟不太安全。
+
+Spring security 还提供了另一种相对更安全的实现机制：在客户端的 Cookie 中，仅保存一个无意义的加密串（与用户名、密码等敏感数据无关），然后在数据库中保存该加密串-用户信息的对应关系，自动登录时，用 Cookie 中的加密串，到数据库中验证，如果通过，自动登录才算通过。
+
+##### 2.2.2.1 实现原理
+
+当浏览器发起表单登录请求时，当通过`UsernamePasswordAuthenticationFilter`认证成功后，会经过 `RememberMeService`，在其中有个`TokenRepository`，它会生成一个 token，首先将 token 写入到浏览器的 Cookie 中，然后将 token、认证成功的用户名写入到数据库中。
+
+当浏览器下次请求时，会经过`RememberMeAuthenticationFilter`，它会读取 Cookie 中的 token，交给 `RememberMeService `从数据库中查询记录。如果存在记录，会读取用户名并去调用`UserDetailsService`，获取用户信息，并将用户信息放入Spring Security 中，实现自动登陆。
+![](https://img-blog.csdnimg.cn/20181202143630639.png)
+
+`RememberMeAuthenticationFilter`在整个过滤器链中是比较靠后的位置，也就是说在传统登录方式都无法登录的情况下才会使用自动登陆。
+
+![](https://img-blog.csdnimg.cn/20181202144420871.png)
+
+##### 2.2.2.2 代码实现
+
+###### 2.2.2.2.1 Token表
+
+首先需要创建一张表来存储 token 信息：
+
+```sql
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+```
+
+###### 2.2.2.2.2 WebSecurityConfir配置
+
+在 WebSecurityConfig 中注入 `dataSource` ，创建一个 `PersistentTokenRepository` 的Bean：
+
+```java
+ @Autowired
+ private DataSource dataSource;
+
+/**
+* 注册一个bean 用于存储token数据
+*
+* @return tokenRepository
+*/
+@Bean
+public PersistentTokenRepository persistentTokenRepository() {
+	JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+	tokenRepository.setDataSource(dataSource);
+	// 如果token表不存在，使用下面语句可以初始化该表；若存在，请注释掉这条语句，否则会报错。
+	//tokenRepository.setCreateTableOnStartup(true);
+	return tokenRepository;
+}
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        // 任何请求 都需要认证
+        // 如果存在不需要认证的url,可以使用 .antMatchers().permitAll()来设置
+        .anyRequest().authenticated()
+        .and()
+        // 设置登录页
+        .formLogin().loginPage("/login")
+        // 设置登录成功也
+        .defaultSuccessUrl("/").permitAll()
+        // 自定义登陆用户名和密码参数，默认为username和password
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .and()
+        // 退出
+        .logout().permitAll()
+        // 自动登录
+        .and()
+        // 自动登录
+        // 只使用 rememberMe 则是保存在token中
+        // 使用tokenRepository后，会产生一个token，与用户名信息进行对应存放在数据库persistent_logins中
+        .rememberMe()
+        // 指定token Repository
+        .tokenRepository(persistentTokenRepository())
+        // 有效时间：单位s
+        .tokenValiditySeconds(60)
+        .userDetailsService(userDetailService);
+    ;
+    // 关闭CSRF跨域
+    http.csrf().disable();
+}
+```
+
+勾选自动登录后，Cookie 和数据库中均存储了 token 信息：
+
+![](https://img-blog.csdn.net/20180509102031410)
--- boot2-security5-module/src/main/java/org/chen/spring/security5/boot/config/WebSecurityConfig.java
+++ boot2-security5-module/src/main/java/org/chen/spring/security5/boot/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package org.chen.spring.security5.boot.config;
 
 import org.chen.spring.security5.boot.service.CustomUserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,10 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * Spring Security 的配置类，该类的三个注解分别是标识该类是配置类、开启 Security 服务、开
@@ -29,6 +34,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailServiceImpl userDetailService;
+    @Autowired
+    private DataSource dataSource;
+
+    /**
+     * 注册一个bean 用于存储token数据
+     *
+     * @return tokenRepository
+     */
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        // 如果token表不存在，使用下面语句可以初始化该表；若存在，请注释掉这条语句，否则会报错。
+        //tokenRepository.setCreateTableOnStartup(true);
+        return tokenRepository;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -65,9 +86,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 // 退出
                 .logout().permitAll()
-                 // 自动登录
-                 .and().rememberMe()
-                 ;
+                // 自动登录
+                .and()
+                // 自动登录
+                // 只使用 rememberMe 则是保存在token中
+                // 使用tokenRepository后，会产生一个token，与用户名信息进行对应存放在数据库persistent_logins中
+                .rememberMe()
+                // 指定token Repository
+                .tokenRepository(persistentTokenRepository())
+                // 有效时间：单位s
+                .tokenValiditySeconds(60)
+                .userDetailsService(userDetailService);
+        ;
         // 关闭CSRF跨域
         http.csrf().disable();
     }
@@ -75,6 +105,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         // 设置拦截忽略文件夹，可以对静态资源放行
-        web.ignoring().antMatchers("/css/**", "/js/**","/favicon.ico","/static/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/favicon.ico", "/static/**");
     }
 }
