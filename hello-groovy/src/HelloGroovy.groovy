22ee2fff73ced04382ced6fbf6be3f62e52c8b0b
chensj(陈世杰) <chensj@winning.com.cn>
2019-09-20 11:18:16 +0800

    gradle init && groovy init

---
+++ hello-groovy/src/HelloGroovy.groovy
@@ -0,0 +1,13 @@
+//class HelloGroovy {
+//    // java 方式在Groovy中书写
+////    public static void main(String[] args) {
+////        System.out.println("Hello Groovy!");
+////    }
+//    // groovy写法
+//    static void main(args){
+//        println "Hello Groovy!"
+//    }
+//
+//}
+// 直接输出  脚本代码
+println "Hello Groovy!"
