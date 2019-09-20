212cb4ea2786fab3c60172ad63eb9c7bee0d59e8
chensj(陈世杰) <chensj@winning.com.cn>
2019-09-20 17:44:36 +0800

    groovy

---
+++ hello-groovy/src/variable/VariableStart.groovy
@@ -0,0 +1,19 @@
+package variable
+
+/*变量学习*/
+
+// 在Groovy中 基本类型都是对象
+int x = 10
+double d = 3.14
+
+println x.class
+println d.class
+
+def a = 1
+def b = 3.14
+def c = 'dddd'
+println(a.class)
+println(b.class)
+println(c.class)
+c = 1.2
+println(c.class)
